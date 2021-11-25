4f0025e34b59f33633564330be4673bf5373cfd7
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2021-11-25 10:30:43 -0500

    Started to work on js.

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const packageXMLPath = 'package.xml';
 const myModule = function () {
   const packageXML = fs.readFileSync(packageXMLPath).toString();
   const customFieldIndex = packageXML.search('CustomField');
-  return console.log(test);
+  if (customFieldIndex !== -1) {
+    const indexOfClosingTypes = packageXML.indexOf('</types>', customFieldIndex);
+    const indexOfTypes = packageXML.LastIndexOf('<types>', customFieldIndex);
+  }
+  return console.log('Custom Fields are not part of the package.xml');
 };
 
 myModule();
