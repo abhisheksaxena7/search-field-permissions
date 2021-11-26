ffb0f588325d41f8664ddce64e1e1585145a9810
Garapati Jyothi Sindhu Sravya <garapati.sravya@wipro.com>
2021-11-26 23:01:25 +0530

    Updated searchInAdminProfile method

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const os = require('os');
 const packageXMLPath = 'package.xml';
 
 const returnCustomFieldsArray = (packageXML, customFieldIndex) => {
-  const filepreceedingindex = packageXML.substr('<types>', customFieldIndex); // package.xml before this index
-  const findtypes = filepreceedingindex.split('<types>').pop(); // package.xml after <types> to customFieldIndex
+  const fileprecedingindex = packageXML.substr('<types>', customFieldIndex); // package.xml before this index
+  const findtypes = fileprecedingindex.split('<types>').pop(); // package.xml after <types> to customFieldIndex
   const indexofmember = findtypes.replace(/ *\<[\S]*?\>/g, ""); // Dispalys only fields
   return indexofmember.trim().split(os.EOL);
 };
 
 const searchInAdminProfile = (customFieldsArray) => {
   return customFieldsArray.forEach((customField) => {
-    console.log('customField = ', customField);
+    const AdminProfilePath = 'force-app/main/default/profiles/Admin.profile-meta.xml';
+    const AdminProfile = fs.readFileSync(AdminProfilePath).toString();
+    const findfield = AdminProfile.includes(customField);
+    const fieldIndex = AdminProfile.search(customField);
+    if (fieldIndex !== -1) {
+      const readableindex = AdminProfile.indexOf('</readable>', fieldIndex);
+      const fileprecedingindex = AdminProfile.substr('<readable>', readableindex);
+      const findtypes = fileprecedingindex.split('<readable>').pop();
+      const str = findtypes.match(true);
+      return str;
+    }
+    else {
+      return console.log('Custom Field should have Visibility access for Admin Profile');
+    }
   });
 };
 
@@ -25,7 +38,6 @@ const searchForFieldPermissions = () => {
     customFieldsArray = returnCustomFieldsArray(packageXML, customFieldIndex);
     return searchInAdminProfile(customFieldsArray);
   }
-
   return console.log('Custom Fields are not part of the package.xml');
 };
 
