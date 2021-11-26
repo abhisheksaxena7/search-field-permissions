4acdef224de3bd2902443e01b938d56877817b42
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2021-11-26 10:19:14 -0500

    Rearrange the files content and add a stub for searchInAdminProfile method.

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const fs = require('fs');
+const os = require('os');
 
 const packageXMLPath = 'package.xml';
-const myModule = function () {
+
+const returnCustomFieldsArray = (packageXML, customFieldIndex) => {
+  const filepreceedingindex = packageXML.substr('<types>', customFieldIndex); // package.xml before this index
+  const findtypes = filepreceedingindex.split('<types>').pop(); // package.xml after <types> to customFieldIndex
+  const indexofmember = findtypes.replace(/ *\<[\S]*?\>/g, ""); // Dispalys only fields
+  return indexofmember.trim().split(os.EOL);
+};
+
+const searchInAdminProfile = (customFieldsArray) => {
+  return customFieldsArray.forEach((customField) => {
+    console.log('customField = ', customField);
+  });
+};
+
+const searchForFieldPermissions = () => {
   const packageXML = fs.readFileSync(packageXMLPath).toString();
   const customFieldIndex = packageXML.search('CustomField');
+  let customFieldsArray = [];
+  // If Custom fields are there in the package.xml
   if (customFieldIndex !== -1) {
-    const indexOfClosingTypes = packageXML.indexOf('</types>', customFieldIndex);
-    const filepreceedingindex = packageXML.substr('<types>', customFieldIndex); // package.xml before this index
-    const findtypes = filepreceedingindex.split('<types>').pop(); // package.xml after <types> to customFieldIndex
-    const indexofmember = findtypes.replace(/ *\<[\S]*?\>/g, ""); // Dispalys only fields
-    const str = indexofmember.trim().split('\r\n');
-    return console.log(str);
+    customFieldsArray = returnCustomFieldsArray(packageXML, customFieldIndex);
+    return searchInAdminProfile(customFieldsArray);
   }
+
   return console.log('Custom Fields are not part of the package.xml');
 };
 
-myModule();
+searchForFieldPermissions();
